a114226b13206bb33ce8875b5e1ecec0a2fc246f
Austin Crosley <atcrosley@gmail.com>
2021-01-19 18:39:50 -0500

    first push to github

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require("dotnev").config();
+let express = require("express");
+let app = express();
+let sequelize = require("./db");
+
+let journal = require("./controllers/journalcontroller");
+let user = require("./controllers/usercontroller");
+
+sequelize.sync();
+//sequelize.sync({force:true})
+// express.json must come before any routes
+app.use(express.json());
+
+app.use("/journal", journal);
+app.use("/user", user);
+
+app.listen(3000, function () {
+  console.log("App is listening on port 3000");
+});
